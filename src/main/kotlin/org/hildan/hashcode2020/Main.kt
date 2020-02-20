f5a0cc181684ab958d1dd101e24ddf7044b76e87
Titouan BION <titouan.bion@gmail.com>
2020-02-20 21:17:44 +0100

    bullshit

--- src/main/kotlin/org/hildan/hashcode2020/Main.kt
+++ src/main/kotlin/org/hildan/hashcode2020/Main.kt
@@ -12,7 +12,27 @@ fun main(files: Array<String>) = runBlocking {
 }
 
 fun HCReader.readProblem(): Problem {
-    val N = readInt()
-    val list = List(N) { readInt() }
-    return Problem(list)
+    val nbBooks = readInt()
+    val nbLibraries = readInt()
+    val scanningTime = readInt()
+    val bookToScore = List(nbBooks) { readInt() }
+    val libs = List(nbLibraries) { index ->  readLibrary(index) }.toMutableList()
+    return Problem(scanningTime, bookToScore, libs)
 }
+
+data class Library(
+    val index: Int,
+    val nbBooks: Int,
+    val signupTime: Int,
+    val nbCanShip: Int,
+    val books: List<Int>
+)
+
+fun HCReader.readLibrary(index: Int): Library {
+    val nbBooks = readInt()
+    val signupLegnth = readInt()
+    val nbCanShip = readInt()
+    val books = List(nbBooks) { readInt() }
+    return Library(index, nbBooks, signupLegnth, nbCanShip, books)
+}
+
--- src/main/kotlin/org/hildan/hashcode2020/Problem.kt
+++ src/main/kotlin/org/hildan/hashcode2020/Problem.kt
@@ -1,9 +1,55 @@
 package org.hildan.hashcode2020
 
+data class OutputLibrary(val index: Int, val books: List<Int>)
+
 class Problem(
-    val items: List<Int>
+    val scanningTime: Int,
+    val bookToScore: List<Int>,
+    var libs: MutableList<Library>,
+    val selectedBooks: MutableSet<Int> = hashSetOf()
 ) {
     fun solve(): Iterable<String> {
-        return items.map { (it * 2).toString() }
+        val output: MutableList<String> = arrayListOf()
+        val filteredLib = createOutput()
+            .filter { it.books.isNotEmpty() }
+
+        output.add(filteredLib.size.toString())
+        filteredLib.forEach {
+            output.add(it.index.toString() + " " + it.books.size)
+            output.add(it.books.joinToString(" "))
+        }
+        return output
+    }
+
+    fun createOutput(): MutableList<OutputLibrary> {
+        val outputLibraries: MutableList<OutputLibrary> = arrayListOf()
+        for (currentTime in 0..scanningTime) {
+            var toto = 0
+            libs = libs
+                .sortedBy {
+                    it.books
+                        .filter { !selectedBooks.contains(it) }
+                        .take(scanningTime - currentTime - it.signupTime)
+                        .sumBy { b -> bookToScore[b] }
+                }.takeWhile {
+                    toto += it.signupTime;
+                    toto <= scanningTime
+                }.toMutableList()
+            if (libs.isEmpty()) {
+                return outputLibraries
+            }
+            val library = libs.removeAt(0)
+
+            val outputLibrary = OutputLibrary(
+                library.index,
+                library.books
+                    .filter { !selectedBooks.contains(it) }
+                    .sortedBy { bookToScore[it] }
+                    .take(scanningTime - currentTime))
+
+            outputLibraries.add(outputLibrary)
+            selectedBooks.addAll(outputLibrary.books)
+        }
+        return outputLibraries
     }
 }
